8220668f740ce902d75b571b0736cc6460d57ac6
James Lee <jamesl33info@gmail.com>
2021-03-21 18:59:12 +0000

    Use the loudnorm filter to normalise audio

--- utils/transcode.go
+++ utils/transcode.go
@@ -15,6 +15,8 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"os/exec"
 	"syscall"
@@ -25,9 +27,87 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// LoudnormStats - Represents the raw stats from the first pass with the loudnorm filter which will be used in the
+// second pass.
+type LoudnormStats struct {
+	MeasuredI         string `json:"input_i"`
+	MeasuredTP        string `json:"input_tp"`
+	MeasuredLRA       string `json:"input_lra"`
+	MeasuredThreshold string `json:"input_thresh"`
+	TargetOffset      string `json:"target_offset"`
+}
+
 // TranscodeFile - Use ffmpeg to transcode the file at the provided path, note that the resulting file will have the
 // '.transcoding.mp4' extension.
 func TranscodeFile(path string) error {
+	lns, err := firstPass(path)
+	if err != nil {
+		return fmt.Errorf("failed to run first pass: %w", err)
+	}
+
+	err = secondPass(path, lns)
+	if err != nil {
+		return fmt.Errorf("failed to run second pass: %w", err)
+	}
+
+	return nil
+}
+
+// firstPass - Run the first pass, this doesn't perform any transcoding; it simply gets the loudnorm stats which will be
+// used in the second pass the achieve the best normalisation results.
+func firstPass(path string) (*LoudnormStats, error) {
+	command := exec.Command(
+		"ffmpeg",
+		"-i",
+		path,
+		"-hide_banner",
+		"-vn",
+		"-af",
+		"loudnorm=print_format=json",
+		"-f",
+		"null",
+		"-",
+	)
+
+	command.SysProcAttr = &unix.SysProcAttr{
+		Pdeathsig: syscall.SIGINT,
+		Setpgid:   true,
+	}
+
+	fields := log.Fields{
+		"path":    path,
+		"command": command.String(),
+	}
+
+	log.WithFields(fields).Debugf("Running first pass")
+
+	output, err := command.CombinedOutput()
+	if err != nil {
+		log.Errorf("%s", output)
+		return nil, fmt.Errorf("failed to run 'ffmpeg': %s", err)
+	}
+
+	split := bytes.Split(output, []byte("\n"))
+	stats := split[len(split)-13:]
+
+	var lns *LoudnormStats
+	err = json.Unmarshal(bytes.Join(stats, []byte("\n")), &lns)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal loudnorm stats: %w", err)
+	}
+
+	fields = log.Fields{
+		"path":           path,
+		"loudnorm_stats": lns,
+	}
+
+	log.WithFields(fields).Debugf("Completed first pass")
+
+	return lns, nil
+}
+
+// secondPass - Run the second pass transcoding the input file using the loudnorm stats from the first pass.
+func secondPass(path string, lns *LoudnormStats) error {
 	command := exec.Command(
 		"ffmpeg",
 		"-i",
@@ -42,6 +122,15 @@ func TranscodeFile(path string) error {
 		"-pix_fmt", "yuv420p",
 		"-acodec", "aac",
 		"-vcodec", "h264",
+		"-af",
+		fmt.Sprintf(
+			"loudnorm=linear=true:measured_i=%s:measured_tp=%s:measured_lra=%s:measured_thresh=%s:offset=%s",
+			lns.MeasuredI,
+			lns.MeasuredTP,
+			lns.MeasuredLRA,
+			lns.MeasuredThreshold,
+			lns.TargetOffset,
+		),
 		ReplaceExtension(path, value.TranscodingExtension),
 	)
 
@@ -49,6 +138,13 @@ func TranscodeFile(path string) error {
 		Pdeathsig: syscall.SIGINT,
 		Setpgid:   true,
 	}
+
+	fields := log.Fields{
+		"path":    path,
+		"command": command.String(),
+	}
+
+	log.WithFields(fields).Debugf("Running second pass")
 
 	output, err := command.CombinedOutput()
 	if err != nil {
